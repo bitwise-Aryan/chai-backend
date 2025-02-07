54d7f72387bb0081426d690aa1da4b5c828d4e34
bitwise-Aryan <aryansingh19july@gmail.com>
2025-02-07 22:21:17 +0530

    ApiError and ApiResponse,AsyncHandler

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "Something went wrong",//overwrite message if something else is passed
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
+
+export {ApiError}
